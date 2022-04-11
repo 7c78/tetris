199256d03566e77c91376edc4dba4e6e81834876
7c78 <jun7c78@gmail.com>
2022-04-12 01:24:28 +0700

    build

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# pong
+# tetris
 
 ## usage
 
--- webpack.build.js
+++ webpack.build.js
@@ -3,18 +3,11 @@ let HtmlWebpackPlugin = require("html-webpack-plugin")
 let CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: "./src/App.fsproj",
-    devtool: "inline-source-map",
     output: {
         path: path.join(__dirname, "./build"),
         filename: "app.js"
-    },
-    devServer: {
-        static: {
-            directory: "./build"
-        },
-        port: 8080
     },
     module: {
         rules: [
